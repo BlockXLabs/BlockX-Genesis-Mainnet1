1d54484889476e06ee248e40699a0ccd0491d58c
benj-defi <ben@tokenizermarkets.com>
2023-12-22 22:06:43 +0800

    edited init.sh command

--- init.sh
+++ init.sh
@@ -21,15 +21,16 @@ rm -rf ~/.blockxd*
 # Reinstall daemon
 make install
 
+cd /root/go/bin
 # Set client config
-blockxd config keyring-backend $KEYRING
-blockxd config chain-id $CHAINID
+./blockxd config keyring-backend $KEYRING
+./blockxd config chain-id $CHAINID
 
 # if $KEY exists it should be deleted
-blockxd keys add $KEY --keyring-backend $KEYRING --algo $KEYALGO
+./blockxd keys add $KEY --keyring-backend $KEYRING --algo $KEYALGO
 
 # Set moniker and chain-id for blockx (Moniker can be anything, chain-id must be an integer)
-blockxd init $MONIKER --chain-id $CHAINID
+./blockxd init $MONIKER --chain-id $CHAINID
 
 # Change parameter token denominations to abcx
 cat $HOME/.blockxd/config/genesis.json | jq '.genesis_time = "2024-01-10T12:00:00.0Z"' > $HOME/.blockxd/config/tmp_genesis.json && mv $HOME/.blockxd/config/tmp_genesis.json $HOME/.blockxd/config/genesis.json
@@ -43,15 +44,15 @@ cat $HOME/.blockxd/config/genesis.json | jq '.app_state["inflation"]["params"]["
 cat $HOME/.blockxd/config/genesis.json | jq '.consensus_params["block"]["max_gas"]="10000000"' > $HOME/.blockxd/config/tmp_genesis.json && mv $HOME/.blockxd/config/tmp_genesis.json $HOME/.blockxd/config/genesis.json
 
 # Allocate genesis accounts (cosmos formatted addresses)
-blockxd add-genesis-account $KEY $MY_TOKENS --keyring-backend $KEYRING
+./blockxd add-genesis-account $KEY $MY_TOKENS --keyring-backend $KEYRING
 
 total_supply=$TOTAL_SUPPLY
 cat $HOME/.blockxd/config/genesis.json | jq -r --arg total_supply "$total_supply" '.app_state["bank"]["supply"][0]["amount"]=$total_supply' > $HOME/.blockxd/config/tmp_genesis.json && mv $HOME/.blockxd/config/tmp_genesis.json $HOME/.blockxd/config/genesis.json
 
 # Sign genesis transaction
-blockxd gentx $KEY $MY_TOKENS --keyring-backend $KEYRING --chain-id $CHAINID
+./blockxd gentx $KEY $MY_TOKENS --keyring-backend $KEYRING --chain-id $CHAINID
 
-blockxd collect-gentxs
+./blockxd collect-gentxs
 cd ~/.blockxd/config
 jq '.app_state.slashing.params.signed_blocks_window = "40000"' genesis.json > temp.json && mv temp.json genesis.json
 jq '.app_state.slashing.params.min_signed_per_window = "0.500000000000000000"' genesis.json > temp.json && mv temp.json genesis.json
@@ -59,4 +60,4 @@ jq '.app_state.slashing.params.slash_fraction_double_sign = "0.08000000000000000
 jq '.app_state.gov.voting_params.voting_period = "1209600s"' genesis.json > temp.json && mv temp.json genesis.json
 jq '.app_state.gov.voting_params.voting_period = "1209600s"' genesis.json > temp.json && mv temp.json genesis.json
 
-blockxd validate-genesis
+./blockxd validate-genesis
